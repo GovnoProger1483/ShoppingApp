8c91cdd3d96c0ad58f8ea131be53c3b0cb04a1bc
sivtsev.107911 <sivtsev.107911@gmail.com>
2020-03-24 23:43:00 +0700

    Добавьте файлы проекта.

---
+++ ShoppingApp.EntityFramework/ShoppingAppDbContext.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShoppingApp.EntityFramework
+{
+    class ShoppingAppDbContext : DbContext
+    {
+        public DbSet<Blog> Blogs { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            options.UseSqlServer("Server=(localdb)\\MSSQLLocalDB;DataBase=MYDB;Trusted_Connection=True;");
+            base.OnConfiguring(options);
+        }
+    }
+
+    public class Blog
+    {
+        public int BlogId { get; set; }
+        public string Url { get; set; }
+    }
+
+}
+
